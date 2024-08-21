1f5b2533f25f79c2a181d313dcc7c1c15daa3e33
wake0up0ne0 <v.osnovnom.bezvreden@gmail.com>
2024-08-21 13:23:10 -0300

    js checked

--- assets/javascript/toggleDark.js
+++ assets/javascript/toggleDark.js
@@ -1,12 +1,13 @@
 var checkbox = document.querySelector('input[name=mode]')
-checkbox.addEventListener('change', function() {
-    if(this.checked) {
+checkbox.addEventListener('change', function () {
+    if (this.checked) {
         document.documentElement.setAttribute('data-theme', 'dark')
         var moon = document.getElementById('moon')
-        moon.innerHTML = feather.icons['sun'].toSvg({'fill': 'var(--fg-color-dark)'})
+        moon.innerHTML = feather.icons['sun'].toSvg({ 'fill': 'var(--fg-color-dark)' })
     } else {
         document.documentElement.setAttribute('data-theme', 'light')
         var moon = document.getElementById('moon')
-        moon.innerHTML = feather.icons['moon'].toSvg({'fill': 'var(--fg-color-dark)'})
+        moon.innerHTML = feather.icons['moon'].toSvg({ 'fill': 'var(--fg-color-dark)' })
     }
 })
+this.checked = true;
