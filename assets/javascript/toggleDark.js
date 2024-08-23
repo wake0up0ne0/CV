722d142d335017c0d00ed8cb57c88c9ef75fc9d2
wake0up0ne0 <v.osnovnom.bezvreden@gmail.com>
2024-08-23 09:28:14 -0300

    finally ?

--- assets/javascript/toggleDark.js
+++ assets/javascript/toggleDark.js
@@ -1,13 +1,18 @@
 var checkbox = document.querySelector('input[name=mode]')
 
 checkbox.addEventListener('change', function () {
-    if (!this.checked) {
+    if (this.checked) {
+        print(checked)
         document.documentElement.setAttribute('data-theme', 'dark')
         var moon = document.getElementById('moon')
         moon.innerHTML = feather.icons['sun'].toSvg({ 'fill': 'var(--fg-color-dark)' })
     } else {
+        print(checked)
         document.documentElement.setAttribute('data-theme', 'light')
         var moon = document.getElementById('moon')
         moon.innerHTML = feather.icons['moon'].toSvg({ 'fill': 'var(--fg-color-dark)' })
     }
 })
+window.onload() {
+    document.documentElement.setAttribute('data-theme', 'dark')
+}
