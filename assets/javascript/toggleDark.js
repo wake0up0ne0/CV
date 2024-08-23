e49edc85e89b1fa6fceb79830d98de2ab7cb29fa
wake0up0ne0 <v.osnovnom.bezvreden@gmail.com>
2024-08-23 09:30:46 -0300

    debug

--- assets/javascript/toggleDark.js
+++ assets/javascript/toggleDark.js
@@ -2,12 +2,12 @@ var checkbox = document.querySelector('input[name=mode]')
 
 checkbox.addEventListener('change', function () {
     if (this.checked) {
-        print(checked)
+        print(this.checked)
         document.documentElement.setAttribute('data-theme', 'dark')
         var moon = document.getElementById('moon')
         moon.innerHTML = feather.icons['sun'].toSvg({ 'fill': 'var(--fg-color-dark)' })
     } else {
-        print(checked)
+        print(this.checked)
         document.documentElement.setAttribute('data-theme', 'light')
         var moon = document.getElementById('moon')
         moon.innerHTML = feather.icons['moon'].toSvg({ 'fill': 'var(--fg-color-dark)' })
